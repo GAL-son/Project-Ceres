b776995d01c4e5b352a33782e513afa99c45f335
GAL_son <pancerek1@gmail.com>
2023-04-13 18:57:04 +0200

    Created EBO class header

---
+++ EBO.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef EBO_CLASS_H
+#define EBO_CLASS_H
+
+#include <glad/glad.h>
+#include <vector>
+
+class EBO
+{
+public:
+	GLuint ID;
+	EBO(std::vector <GLuint> indecies);
+
+	void Bind();
+	void Unbind();
+	void Delete();
+};
+
+#endif // !EBO_CLASS_H
+
