69f6770e95bf1f3ea6e0f74bd471ff3963224e19
GAL_son <pancerek1@gmail.com>
2023-04-17 15:48:51 +0200

    VBO constructor implemented

---
+++ VBO.cpp
@@ -0,0 +1,8 @@
+#include "VBO.h"
+
+VBO::VBO(std::vector <Vertex>& vertices)
+{
+	glGenBuffers(1, &ID);
+	glBindBuffer(GL_ARRAY_BUFFER, ID);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), GL_STATIC_DRAW);
+}
